4322bd606d78e2ba7e7103d08903f15335721c68
Miguel Gil F <67928892+Miguel-web@users.noreply.github.com>
2023-07-12 16:27:12 -0700

    Initial commit

---
+++ src/js/component/Todolist.jsx
@@ -0,0 +1,42 @@
+import React, {useState} from 'react'
+// https://assets.breatheco.de/apis/fake/todos/user/miguelgilweb
+
+const TodoList = () => {
+    const [task, setTask] = useState("");
+    const [listTask, setListTask] = useState([]);
+    const handleTask = (event) => {
+        setTask(event.target.value); 
+    } 
+    const addTask = (event) => {
+        if (event.key == "Enter") {
+            setListTask([...listTask, task]);
+            setTask("");
+        }
+    }
+    const handlerButtomDelete = (indexid) => setListTask(listTask.filter((task, index)=> (indexid != index)))
+
+    return (
+        <div className="text-center text-white d-flex align-items-center gap-3 flex-column">
+        <h1>Todo List</h1>
+            <input className="text-dark" type="text" id="task" value={task} onKeyDown={addTask} onChange={handleTask} placeholder=' What needs to be done?'/>  
+            <div>
+                <ul>
+                {
+                    listTask.map((task, index) => {
+                        return (
+                        <li key={index}> <h3>{task} <button type='button' className='btn btn-close' onClick={(event) => handlerButtomDelete(index)}></button></h3> </li>
+                        )
+                    })
+                }
+                </ul>
+            </div>
+            <div className='m-1'> <p>{listTask.length === 0
+              ? "No tasks, (😊Hola Jose...)"
+              : listTask.length + " Item Left👍"}</p></div>    
+        </div>
+        
+  );
+};
+
+export default TodoList;
+
---
+++ src/js/component/home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TodoList from "./Todolist"
+
+//create your first component
+const Home = () => {
+	return (
+		<div className="Container">
+			<TodoList/>
+		</div>
+	);
+};
+export default Home;
